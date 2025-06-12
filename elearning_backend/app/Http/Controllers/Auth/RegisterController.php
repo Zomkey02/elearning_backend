f09eddebee8f632fb652198bd01432fed46388c2
Zomkey02 <zomkey.wien@hotmail.com>
2025-06-12 16:30:21 +0200

    add fortify, add registercontroller but check only with Insomnia basic connection - no use of fortify for registration,

---
+++ elearning_backend/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+//use Laravel\Fortify\Rules\Password as PasswordRule;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+//use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request) 
+    {
+        $request->validate([
+            'username' => ['required', 'string', 'min:2', 'max:255', Rule::unique(User::class)],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique(User::class)],
+            'password' => ['required', 'string', 'confirmed', Password::min(8)->mixedCase()->numbers()->symbols()],
+        ]);
+
+        /* $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User registered successfully.',
+            'user' => $user
+        ], 201); */
+        dd('register');
+    }
+}
--- elearning_backend/routes/api.php
+++ elearning_backend/routes/api.php
@@ -3,6 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::post('/register', [RegisterController::class, 'register']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
