d3c81c10883b32b054aee577a8f281667ce7b6d9
Zomkey02 <zomkey.wien@hotmail.com>
2025-06-13 21:29:10 +0200

    created logoutController, works almost perfect, API tested

--- elearning_backend/routes/api.php
+++ elearning_backend/routes/api.php
@@ -5,25 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 
 
-
-
-
-
-
-
-
-
-Route::get('test', function () {
+/* Route::get('test', function () {
     return response()->json(['message' => 'API is working']);
-});
+}); */
 
 
 Route::post('/register', RegisterController::class);
-
 Route::post('/login', LoginController::class);
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
@@ -31,5 +24,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return $request->user();
     });
 
-});
+    Route::post('/logout', LogoutController::class);
 
+});
