5407a0107c8ac5756b6167e5a115e7df04e7279e
Zomkey02 <zomkey.wien@hotmail.com>
2025-08-13 02:24:35 +0200

    add delete to cors

--- elearning_backend/config/cors.php
+++ elearning_backend/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'update', 'user', 'register'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'update', 'user', 'register', 'delete'],
 
     'allowed_methods' => ['*'],
 
